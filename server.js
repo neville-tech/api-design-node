5d8bd1386ecde4efe7a5e7584f7ce6f2fb91e288
Neville Bokdawalla <nevillekb@gmail.com>
2018-07-14 16:45:18 +0530

    updated package.json, added package.lock.json, added code to require the express module and create a basic http server that serves and html page and json data based on the routes passed respectively

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
 
-var jsonData = {count: 12, message: 'hey'};
+var jsonData = { count: 12, message: 'hey' };
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html", function (err) {
+        if (err) {
+            console.log("error occurred while sending the file..", err)
+        }
+    })
+});
+
+app.get('/data', function (req, res) {
+    //res.send(jsonData);
+    res.json(jsonData);
+});
+
+var port = 3000;
+app.listen(port, function () {
+    console.log("listening on port number", port)
+})
