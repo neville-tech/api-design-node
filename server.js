47ef3107e47cdbaa5f6aef36357500970b3d3d74
Neville Bokdawalla <nevillekb@gmail.com>
2018-07-14 21:19:11 +0530

    added routes for get, post, put and delete. made use of body parser for parsing the request body as json

--- server.js
+++ server.js
@@ -1,25 +1,74 @@
-// TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var jsonData = { count: 12, message: 'hey' };
+// Middleware
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html", function (err) {
-        if (err) {
-            console.log("error occurred while sending the file..", err)
+//support the parsing of application/json
+app.use(bodyParser.json())
+
+//some movie data
+var movies = [
+    {
+        "id": 1,
+        "name": "The dark knigt"
+    },
+    {
+        "id": 2,
+        "name": "Inception"
+    }
+]
+
+// Routes
+
+// GET /movies
+app.get('/movies', function (req, res) {
+    res.send(movies);
+});
+
+// GET /movies/id
+app.get('/movies/:id', function (req, res) {
+
+    var reqId = req.params.id;
+    var result;
+
+    movies.forEach(function (movie) {
+        if (movie.id == reqId) {
+            result = movie;
         }
-    })
+    });
+
+    if (result) {
+        res.send(result);
+    }
+    else {
+        res.sendStatus(404);
+    }
 });
 
-app.get('/data', function (req, res) {
-    //res.send(jsonData);
-    res.json(jsonData);
+// POST /movies
+app.post('/movies', function (req, res) {
+
+    var movieData = req.body;
+    movies.push(movieData);
+
+    res.send(movies);
 });
 
+// PUT /movies/id
+app.put('/movies/:id', function (req, res) {
+
+
+})
+
+// DELETE /movies/id
+app.delete('/movies/:id', function (req, res) {
+
+});
+
+//listen
 var port = 3000;
 app.listen(port, function () {
     console.log("listening on port number", port)
-})
+});
